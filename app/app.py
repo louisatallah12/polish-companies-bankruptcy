885e31cdae1c579ccda510f093d2af45246e122b
louis <louis.atallah@icloud.com>
2024-11-13 18:56:30 -0700

    wip - working but needs improvements on user experience

---
+++ app/app.py
@@ -0,0 +1,56 @@
+from flask import Flask, request, flash, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+import joblib 
+import pandas as pd
+from forms import MyForm
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'my secret key'
+
+Bootstrap(app)
+
+@app.route('/index')
+def index():
+    return render_template('index.html')
+
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('error.html')
+
+
+model = joblib.load("../LR")
+@app.route('/predict', methods=['GET', 'POST'])
+def predict():
+    form = MyForm(request.form)
+    message=''
+    liste = []
+
+    if request.method == 'POST'  :        
+        
+        liste.append(form.Attr16.data)
+        liste.append(form.Attr25.data)
+        liste.append(form.Attr33.data)
+        liste.append(form.Attr40.data)
+        liste.append(form.Attr46.data)
+        liste.append(form.Attr63.data)
+        liste.append(form.year.data)
+
+        p = model.predict(pd.DataFrame([liste]))[0]
+        if p == 1 :
+            message = "Sorry... it seems that your company go straight to bankruptcy :("
+        elif p == 0 : 
+            message = "Your company will not go to bankruptcy! :)"
+        
+        flash(message)
+        return redirect(url_for('index'))
+    else :
+
+        return render_template('pred.html', form=form)
+
+
+if __name__ == "__main__" : 
+    app.run(
+        host = "127.0.0.1", 
+        port=5000,
+        debug=True
+    )
---
+++ app/forms.py
@@ -0,0 +1,21 @@
+from wtforms.validators import DataRequired, InputRequired
+from wtforms import FloatField, SubmitField, IntegerField
+from flask_wtf import FlaskForm
+from flask import request
+
+
+class MyForm(FlaskForm):
+    year = IntegerField("year", validators= [ DataRequired()])
+    Attr63 = FloatField("sales / short-term liabilities", validators=[ DataRequired()])
+    Attr46 = FloatField("(current assets - inventory) / short-term liabilities", validators=[ DataRequired()])
+    Attr40 = FloatField("(current assets - inventory - receivables) / short-term liabilities", validators=[ DataRequired()])
+    Attr33 = FloatField("operating expenses / short-term liabilities", validators=[ DataRequired()])
+    Attr25 = FloatField("(equity - share capital) / total assets", validators=[ DataRequired()])
+    Attr16 = FloatField("(gross profit + depreciation) / total liabilities", validators=[ InputRequired()])
+
+    submit = SubmitField("Submit")
+
+    def validate_on_submit(self) : 
+        if request.method == 'POST' and self.validate() : 
+            return True
+        return False
